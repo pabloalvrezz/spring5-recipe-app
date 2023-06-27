3fab971395960107d1e7650d768c50a7cf837e96
Pablo Alvarez Gonzalez <pagonzalez@izertis.com>
2023-06-27 09:35:51 +0200

    Añadimos las relaciones ManyToMany

--- src/main/java/guru/domain/Difficulty.java
+++ src/main/java/guru/domain/Difficulty.java
@@ -1,4 +1,4 @@
-package domain;
+package guru.domain;
 
 public enum Difficulty {
 	EASY, MODERATE, DIFFICULT;
--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -2,8 +2,10 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = { "guru.domain" })
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
